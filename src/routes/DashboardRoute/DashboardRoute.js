b0880bccb3e44c075f99260a23a636a0731d2f11
quonn <qber83@gmail.com>
2019-05-29 12:27:55 -0700

    removed state and added methods to componentWillMount

--- src/routes/DashboardRoute/DashboardRoute.js
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react'
 import languageService from '../../services/language-service'
 import LanguageContext from '../../contexts/LanguageContext'
+import { Link } from 'react-router-dom'
 import "../../components/App/App.css"
 
 class DashboardRoute extends Component {
-
-  state = {
-    words: [{}]
-  }
 
   static contextType = LanguageContext;
 
@@ -19,31 +16,48 @@ class DashboardRoute extends Component {
     languageService.getLanguage()
       .then(language => (language.words))
       .then(this.context.setWords)
+
+      languageService.getLanguageHead()
+      .then(totalScore =>  totalScore.totalScore)
+      .then(this.context.setTotalScore)
+
+      languageService.getLanguageHead()
+      .then(wordCorrectCount =>  wordCorrectCount.wordCorrectCount)
+      .then(this.context.setWordCorrectCount)
+
+      languageService.getLanguageHead()
+      .then(wordIncorrectCount =>  wordIncorrectCount.wordIncorrectCount)
+      .then(this.context.setWordIncorrectCount)
   }
 
+  
   renderWords() {
     let { words = [] } = this.context
-
+    let { wordIncorrectCount, wordCorrectCount } = this.context
     return words.map(word => {
-      console.log(word)
+      console.log()
       return <>
-        <p>{word.original}</p>
+        <li>
+          <h4>{word.original}</h4>
+          correct answer count: {wordCorrectCount}
+          incorrect answer count: {wordIncorrectCount}
+        </li>
       </>
     }
     )
-
   }
 
   render() {
     const subtitle = this.context.language
+    let { totalScore } = this.context 
     let words = this.renderWords()
     return (
       <section>
-        <h3>{subtitle}</h3>
-        <h1 className="dashboardScore">Score:{` `}</h1>
-        <h4>Words to practice:</h4>
-        {words}
-        <button className="btn">Start Learning</button>
+        <h2>{subtitle}</h2>
+        <h1 className="dashboardScore">Total correct answers: {totalScore}</h1>
+        <h3>Words to practice</h3>
+        <ul>{words}</ul>
+        <a href='/learn' className="btn">Start practicing</a>
       </section>
     );
   }
