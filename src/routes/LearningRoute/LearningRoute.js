b65ef06c47736408d0add31ab35a6607d182abeb
Farah Student <farah.gus@hotmail.com>
2019-05-29 09:27:38 -0400

    changes file name

--- src/routes/LearningRoute/LearningRoute.js
+++ src/routes/LearningRoute/LearningRoute.js
@@ -1,49 +1,61 @@
 import React, { Component } from 'react'
 import languageService from '../../services/language-service'
+import LanguageContext from '../../contexts/LanguageContext'
 import { Input, Required, Label } from '../../components/Form/Form'
 import Button from '../../components/Button/Button'
 import "../../components/App/App.css"
 
 class LearningRoute extends Component {
-  state = {
-    language: [],
-    words: [],
-    nextWord: []
-  }
+
+  static contextType = LanguageContext
+
+  // state = {
+  //   language: [],
+  //   words: [],
+  //   nextWord: []
+  // }
   
-  getLanguage = () => {
-    languageService.getLanguage()
-      .then(res => {
-        this.setState({
-          language: res.language,
-          words: res.words
-        })
-        console.log(this.state.words)
-      })
-  }
+  // Don't think I need getLanguage()
+  // getLanguage = () => {
+  //   languageService.getLanguage()
+  //     .then(res => {
+  //       this.setState({
+  //         language: res.language,
+  //         words: res.words
+  //       })
+  //       console.log(this.state.words)
+  //     })
+  // }
 
-  getLanguageHead = () => {
-    languageService.getLanguageHead()
-      .then(res => {
-        this.setState({
-          nextWord: [...this.state.nextWord, res]
-        })
-      })
-  }
+  // getLanguageHead = () => {
+  //   languageService.getLanguageHead()
+  //     .then(res => {
+  //       this.setState({
+  //         nextWord: [...this.state.nextWord, res]
+  //       })
+  //       console.log(this.state.nextWord)
+  //     })
+  // }
 
-  componentDidMount() {
-    this.getLanguage();
-    this.getLanguageHead();
-  }
+  // componentDidMount() {
+  //  // this.getLanguage();  don't think I need this
+  //   this.getLanguageHead();
+  // }
 
-  displayWord = () => {
-    const word = this.state.nextWord.map(word => 
-      <h2>How do you say "{word.nextWord}"</h2>
-      )
-    return word;
-  }
+  // displayWord = () => {
+  //   const word = this.state.nextWord.map((word, i) => 
+  //     <h2 key={i}>How do you say "{word.nextWord}"</h2>
+  //     )
+  //   return word;
+  // }
 
-  render() {    
+  // handleSubmitAnswer = (e) => {
+  //   e.preventDefault();
+  //   const { answer } = e.target
+  // }
+
+  render() {
+    console.log(this.context)
     
     return (
       <section className="quiz-wrapper">
@@ -51,7 +63,7 @@ class LearningRoute extends Component {
           Status bar
         </div>
         <article className="learning-quiz-question-box">
-          {this.displayWord()}
+          {/* {this.displayWord()} */}
         </article>
         <div className="answer-form">
           <form>
@@ -62,7 +74,7 @@ class LearningRoute extends Component {
               className="translation-input"
               ref={this.firstInput}
               id='translation-input'
-              name='translation'
+              name='answer'
               required
               />
             <Button type='submit' className="btn">
